96333d35a42d83b22a90ce7035bace8c8e07e3d8
Dhruv7-code <dhruvprj1001@gmail.com>
2026-01-09 10:30:43 -0700

    Changed the image path for the project images

--- src/data/projects.js
+++ src/data/projects.js
@@ -4,7 +4,7 @@ export const projects = [
   {
     id: 1,
     title: "Sound — Minimal Music Streaming Platform",
-    image: "./public/images/projects/music.png",
+    image: "./images/projects/music.png",
     category: "Full Stack Web Application",
     technologies: [
       "HTML5",
@@ -22,7 +22,7 @@ export const projects = [
   {
     id: 2,
     title: "Personal Portfolio Website",
-    image: "./public/images/projects/portfolio.png",
+    image: "./images/projects/portfolio.png",
     category: "Frontend Development",
     technologies: [
       "React",
@@ -41,7 +41,7 @@ export const projects = [
   {
     id: 3,
     title: "Business Intelligence & Marketing Insights System",
-    image: "./public/images/projects/data_analysis.png",
+    image: "./images/projects/data_analysis.png",
     category: "Data Analytics & Business Intelligence",
     technologies: [
       "Python",
